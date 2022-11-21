05d5d891d6d003c0be1f54b35621376008c94e7f
Erick Pacheco <58119210+erick-pacheco@users.noreply.github.com>
2022-11-21 00:40:32 +0000

    Create calculate and use methods and pass tests

--- Calculator.java
+++ Calculator.java
@@ -1,23 +1,40 @@
-public class Calculator{
-  
-  public static void throwANumber(int throwMe)throws Exception{
+public class Calculator {
+
+  public static void throwANumber(int throwMe) throws Exception {
     throw new Exception("I don't like the number " + throwMe + ", you can have it back!");
   }
 
-  public static void catchIt(int catchMe){
-    try{
+  public static void catchIt(int catchMe) {
+    try {
       throwANumber(catchMe);
-    }catch(RuntimeException ex){
-      System.out.println("You'll never see this!");
-    }catch(Exception e){
-      System.out.println(e.getMessage());
-    }finally{
-      System.out.println("I'm in the finally block");
+    } catch (RuntimeException ex) {
+      log("You'll never see this!");
+    } catch (Exception e) {
+      log(e.getMessage());
+    } finally {
+      log("I'm in the finally block");
     }
   }
 
-  public static void calculate(int x, int y) {
-    
+  // Your code will go here
+  public static void calculate(int x, int y) throws ArithmeticException {
+    log(x / y);
   }
-  
+
+  public static void use(int x, int y)  {
+    try {
+      calculate(x, y);
+    } catch (ArithmeticException ex) {
+      log("Don't divide by 0!");
+    } catch (Exception ex) {
+      log("Some other exception");
+    } finally {
+      log("This will always print out");
+    }
+  }
+
+  public static void log(Object message) {
+    System.out.println(message);
+  }
+
 }
